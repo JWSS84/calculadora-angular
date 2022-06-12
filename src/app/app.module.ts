2343c7ce7dd747732213fb598f50af8c7b2e0834
jwss84 <jarbaswssilva@gmail.com>
2022-06-12 15:36:05 -0300

    criação do projeto, primeiros passos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+
+    AppRoutingModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
