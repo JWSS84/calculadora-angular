0b7032306edc172b7e38ca0f5bc37dc392990f79
jwss84 <jarbaswssilva@gmail.com>
2022-06-15 08:33:05 -0300

    implementando funcao no services

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -4,6 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculadoraService {
+  
+  static readonly SUM: string = '+';
+  static readonly SUB: string = '-';
+  static readonly DIV: string = '/';
+  static readonly MULT: string = '*';
 
   constructor() { }
+
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number;
+
+    switch (operacao) {
+      case CalculadoraService.SUM:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUB:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIV:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULT:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+  }
 }
